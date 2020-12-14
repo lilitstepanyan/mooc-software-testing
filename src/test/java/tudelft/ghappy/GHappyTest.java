39c9026014d253055a07240c4c7d1e4d26e2cdfa
LILIT <LILIT@10.0.0.50>
2020-12-13 20:15:38 -0800

    Practical exercise #1 Bug found and fixed.

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,20 @@
 package tudelft.ghappy;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+	boolean result;
+	
+	@Test
+	public void testExamples() {
+		GHappy gh = new GHappy();
+		gh.gHappy("xxggxx");
+		assertTrue(true);
+		gh.gHappy("xxgxx");
+		assertTrue(true);
+		gh.gHappy("xxggyygxx");
+		assertTrue(true);
+	}
 }
